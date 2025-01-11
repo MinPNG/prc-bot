f01a104540065d4762af980726f3c7e802a6ecc8
MinPNG <minhphongdn1@yahoo.com>
2025-01-11 22:32:58 +0100

    Now let the user pick the alert channel

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os import getenv
 from dotenv import load_dotenv
 import discord
-from function import add,remove,change,check,reset,do,undo,help,jp_tzone
+from function import add,remove,change,check,reset,do,undo,get,help,jp_tzone
 import time
 import datetime
 from discord.ext import commands,tasks
@@ -20,7 +20,7 @@ class MyClient(discord.Client):
         print(f'Message from {message.author}: {message.content} in {message.channel}')
         try:
             (action, arg) = respone_handler(arg)
-            await action_handler(message,action,arg)
+            await self.action_handler(message,action,arg)
             # await message.channel.send("This is an automated reply")
         except Exception as e:
             print(e)
@@ -29,6 +29,20 @@ class MyClient(discord.Client):
     async def my_task(self):
         message = check([])[1]
         await self.channel.send(message)
+
+    async def action_handler(self,msg,action,*args):
+        match(action):
+            case "assign":
+                dict = args[0]
+                if "channel" in dict.keys():
+                    channel_id = dict["channel"][2:-1]
+                    print(channel_id)
+                    self.channel = self.get_channel(int(channel_id))
+                    await msg.channel.send("Channel = " + channel_id)
+            case "reply":
+                await msg.channel.send(args[0])
+            case "react":
+                await msg.add_reaction(args[0])
 
 def message_handler(message):
     arg = message.split(' ')
@@ -44,40 +58,18 @@ def respone_handler(arg):
         return func(arg)
     return ("reply","Something wrong, try again")
 
-async def action_handler(msg,action,*args):
-    match(action):
-        case "reply":
-            await msg.channel.send(args[0])
-        case "react":
-            await msg.add_reaction(args[0])
-
 async def day_reset():
     while(True):
         if (time.time()["hour"] > 9):
             print("Time reset")
-
-#Class to handle the event that run every 4:30AM JST
-class Alert(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.channel = bot.get_channel(1306661987144564756)
-        self.my_task.start()
-
-    def cog_unload(self):
-        self.my_task.cancel()
-
-    @tasks.loop(time=alert_time)
-    async def my_task(self):
-        message = check('')[1]
-        await self.channel.send(message)
 
 if __name__ == "__main__":
 
     intents = discord.Intents.default()
     intents.message_content = True
 
-    name_list = ["add","rm","change","check","reset","do","undo","help"]
-    func_list = [add,remove,change,check,reset,do,undo,help]
+    name_list = ["add","rm","change","check","reset","do","undo","get","help"]
+    func_list = [add,remove,change,check,reset,do,undo,get,help]
 
     load_dotenv()
     TOKEN = getenv('TOKEN')
